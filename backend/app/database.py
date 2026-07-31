fe76486d1c4c20be813a0666c746680d6492cfd1
TanvikaOjha <25je0806@iitism.ac.in>
2026-07-31 14:55:12 +0530

    added database connection

---
+++ backend/app/database.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres@localhost:5432/collabdb")
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+class Base(DeclarativeBase):
+    pass
+
+def get_db():
+    db = SessionLocal()
+    try: 
+        yield db
+    finally: db.close()    
